48866bea78100df6079f62128552118ad0cc85a6
hongzhe12 <hongzhe2022@163.com>
2026-08-09 08:16:27 +0800

    first

---
+++ build.sh
@@ -0,0 +1,4 @@
+pyinstaller \
+    --onefile \
+    --name vscode-installer \
+    install.py
---
+++ install.py
@@ -0,0 +1,50 @@
+from pathlib import Path
+import tarfile
+import shutil
+
+
+def install(commit, cli_tar, server_tar):
+    base = Path.home() / ".vscode-server"
+
+    # 1. 放置 CLI 标记文件
+    cli_target = base / f"vscode-cli-{commit}.tar.gz.done"
+    base.mkdir(parents=True, exist_ok=True)
+
+    shutil.copy(
+        cli_tar,
+        cli_target
+    )
+
+    # 2. 解压 Server
+    server_dir = (
+        base /
+        "cli" /
+        "servers" /
+        f"Stable-{commit}" /
+        "server"
+    )
+
+    server_dir.mkdir(parents=True, exist_ok=True)
+
+    with tarfile.open(server_tar) as tar:
+        tar.extractall(
+            path=server_dir,
+            filter="data"
+        )
+
+    # 3. 创建兼容软链接
+    bin_dir = base / "bin"
+    bin_dir.mkdir(exist_ok=True)
+
+    link = bin_dir / commit
+
+    if not link.exists():
+        link.symlink_to(server_dir)
+
+
+if __name__ == "__main__":
+    install(
+        commit="fdb98833154679dbaa7af67a5a29fe19e55c2b73",
+        cli_tar="/tmp/vscode_cli_alpine_x64_cli.tar.gz",
+        server_tar="/tmp/vscode-server-linux-x64.tar.gz"
+    )
